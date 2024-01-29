1ea03d0be40736a69287e5b30e59e948196080da
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2024-01-29 23:54:28 +0300

    task-3

--- 0x00-pagination/3-hypermedia_del_pagination.py
+++ 0x00-pagination/3-hypermedia_del_pagination.py
@@ -38,6 +38,7 @@ class Server:
                 i: dataset[i] for i in range(len(dataset))
             }
         return self.__indexed_dataset
+
     def get_hyper_index(self, index: int = None, page_size: int = 10) -> Dict:
         """Retrieves info about a page from a given index and with a
         specified size.
